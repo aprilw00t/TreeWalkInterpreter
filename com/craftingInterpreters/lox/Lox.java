7f094d027faa0942cb79ff874ca946b981e88aaa
April <apriljoy040370@hotmail.co.uk>
2023-07-31 14:53:59 +0100

    up to regular languages and expressions

---
+++ com/craftingInterpreters/lox/Lox.java
@@ -0,0 +1,7 @@
+package com.craftingInterpreters.lox;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ com/craftingInterpreters/lox/TokenType.java
@@ -0,0 +1,2 @@
+package com.craftingInterpreters.lox;public class TokenType {
+}
